a7248b721c14023bad5f116e83898eda577bd181
runoob <test@runoob.com>
2020-12-05 10:06:48 +0800

    django url route distribution

--- django_search_engine/django_search_engine/urls.py
+++ django_search_engine/django_search_engine/urls.py
@@ -13,9 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path
 
+from search_engine_app import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^$', views.index, name='index'),
+    url(r'^index/$', views.index, name='index'),
+    url(r'^result/$', views.result, name='result'),
 ]
